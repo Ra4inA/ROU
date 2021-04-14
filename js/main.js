31bc4496ea363325783822a6e76747dc18313e38
Ra4inA <vitya-sorokin-92@mail.ru>
2021-04-14 20:43:04 +0300

    final

--- js/main.js
+++ js/main.js
@@ -40,3 +40,49 @@ if(animItems.length > 0) {
 		animOnScroll();
 	}, 300);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+function Spoiler1() {
+	var ele = document.getElementById("contentSpoiler1");
+	var text = document.getElementById("linkSpoiler1");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+  	}
+	else {
+		ele.style.display = "block";
+	}
+}
+
+
+function Spoiler2() {
+	var ele = document.getElementById("contentSpoiler2");
+	var text = document.getElementById("linkSpoiler2");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+  	}
+	else {
+		ele.style.display = "block";
+	}
+}
+
+
+function Spoiler3() {
+	var ele = document.getElementById("contentSpoiler3");
+	var text = document.getElementById("linkSpoiler3");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+  	}
+	else {
+		ele.style.display = "block";
+	}
+}
